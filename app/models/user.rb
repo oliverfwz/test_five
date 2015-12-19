070cd3e3f5866608087cadceaffef19e35032f27
Oliver Futureworkz <oliver@futureworkz.com>
2015-12-19 11:43:44 +0700

    Build #age in Student + rspec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   validates :name, presence: true
   validates :dob, presence: true
+
+  def age
+  	return if dob.blank?
+  	((Date.today - dob) / 365).to_i
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,12 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:dob) }
   end
+
+  context 'Show age'  do
+  	let!(:student) { create(:student, dob: (Date.today - 24.years)) }
+
+  	it 'return age' do
+  		expect(student.age).to eq 24
+  	end
+  end
 end
