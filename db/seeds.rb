5e0811272c17b76f3785c7797f0aee8eaa2cda2a
Oliver Futureworkz <oliver@futureworkz.com>
2015-12-19 12:38:01 +0700

    Update db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+School.destroy_all
+
+university  = University.create!(name: 'Univer', address: '123 AAA')
+high_school = HighSchool.create!(name: 'High School', address: '123 AAA')
+
+university.students.create!(name: "Oliver", dob: "29-09-1991")
+university.students.create!(name: "Theo", dob: "29-09-1991")
+university.students.create!(name: "Smith", dob: "29-09-1991")
+
+high_school.students.create!(name: "Alex", dob: "29-09-1991")
+high_school.students.create!(name: "Robin", dob: "29-09-1991")
+high_school.students.create!(name: "Yunchang", dob: "29-09-1991")
+
+OfficeWorker.create!(name: "Sophie", dob: "1-1-1993", years_of_exp: 2)
+OfficeWorker.create!(name: "Heroman", dob: "1-1-1989", years_of_exp: 3)
