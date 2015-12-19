a78a87cc9348b01f4bbda2c8abfc63cdcd696337
Oliver Futureworkz <oliver@futureworkz.com>
2015-12-19 12:13:22 +0700

    Update validate association in school model

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,4 +1,7 @@
 class School < ActiveRecord::Base
   validates :name, presence: true
   validates :address, presence: true
+
+  has_many :school_users, dependent: :destroy
+  has_many :students, through: :school_users
 end
--- spec/models/school_spec.rb
+++ spec/models/school_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe School, type: :model do
+  context 'Association' do
+    it { is_expected.to have_many(:school_users) }
+    it { is_expected.to have_many(:students) }
+  end
+
   context 'Validation' do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:address) }
