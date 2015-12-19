5e0ea8a977abbccef6f6421e24a6a71b2e20e284
Oliver Futureworkz <oliver@futureworkz.com>
2015-12-19 12:12:24 +0700

    Update validate in school_user

--- app/models/school_user.rb
+++ app/models/school_user.rb
@@ -1,4 +1,4 @@
 class SchoolUser < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :student, foreign_key: "user_id"
   belongs_to :school
 end
--- spec/models/school_user_spec.rb
+++ spec/models/school_user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe SchoolUser, type: :model do
   context 'Association' do
-    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:student) }
     it { is_expected.to belong_to(:school) }
   end
 end
